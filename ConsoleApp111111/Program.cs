4df9049359f03ce2f445e217ebc084fd5ac1ffd7
volga <volga@DESKTOP-89UV4JU>
2024-09-21 17:39:38 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp111111/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Intrinsics;
+
+
+
+namespace ConsoleApp111111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            string str = "";
+            Console.Write("Enter number -> ");
+            str = Console.ReadLine();          
+            int n;           
+                int.TryParse(str, out n);
+            if (n != 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            
+
+
+        }
+    }
+}
