67bd42ef33ee70651df195a8e2e5b09f31bedd43
volga <volga@DESKTOP-89UV4JU>
2024-09-22 14:37:30 +0500

    last

--- ConsoleApp111111/Program.cs
+++ ConsoleApp111111/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.Runtime.Intrinsics;
+using System.Runtime.Serialization;
 
 
 
@@ -8,6 +11,7 @@ namespace ConsoleApp111111
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
 
@@ -59,15 +63,15 @@ namespace ConsoleApp111111
              */
             Console.WriteLine("\n\n\n\t\tEx3\n");
             string strName, strSurname, strTemp;
-            Console.Write("Enter SURNAME -> ");
+            Console.Write("\tEnter SURNAME -> ");
             strName = Console.ReadLine();
-            Console.Write("Enter Name -> ");
+            Console.Write("\tEnter Name -> ");
             strSurname = Console.ReadLine();
-            Console.WriteLine("1st wrong" + strName + " " + strSurname);
+            Console.WriteLine("\t1st wrong " + strName + " " + strSurname);
             strTemp = strName;
             strName = strSurname;
             strSurname = strTemp;
-            Console.WriteLine("2nd right" + strName + " " + strSurname);
+            Console.WriteLine("\t2nd right " + strName + " " + strSurname);
             /*
              Задание 4:
         Вы приходите в магазин и хотите купить за своё золото кристаллы. В вашем кошельке есть
@@ -79,6 +83,31 @@ namespace ConsoleApp111111
         кристаллов выводится на экран.
              */
             Console.WriteLine("\n\n\n\t\tEx4\n");
+            Console.Write("\tСколько у вас золота?-> ");
+            int nG = Convert.ToInt32(Console.ReadLine());
+            Random r = new Random();
+            int randPrice = r.Next(1,nG);
+            Console.Write("\tСколько хотите купит кристаллов по ценне "+randPrice+" ? -> ");
+            int nC = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\tВы купили " + nC+ " кристаллов " + " золота осталось " + (100-(nC*randPrice)));
+            /*
+             Задание 5:
+        Вы заходите в поликлинику и видите огромную очередь из старушек, вам нужно рассчитать
+        время ожидания в очереди.
+        Пользователь вводит кол-во людей в очереди.
+        Фиксированное время приема одного человека всегда равно 10 минутам.
+        Пример ввода: Введите кол-во старушек: 14
+        Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут."
+             */
+            Console.WriteLine("\n\n\n\t\tEx5\n");
+            int counter = 0;
+            Console.Write("\tВведите кол-во старушек -> ");
+            int numStarushka = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < numStarushka; i++)
+            {
+                counter++;
+            }
+            Console.WriteLine("\tВам ждать " + counter*10/60 + " часов и " + counter*10%60 + " минут");
         }
     }
 }
